367471c74ab5a8e676300ff4a97e9b3c8e97be7f
audrain simon <simondu06730@hotmail.fr>
2022-09-29 22:25:17 +0200

    para

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,32 +1,10 @@
 import React, { useRef, useEffect } from "react";
 import "../Description/Description.scss";
-import gsap from "gsap-trial";
-import { SplitText } from "gsap-trial/SplitText";
- 
+
 function Description() {
   const para = useRef(null);
-  gsap.registerPlugin(SplitText);
 
-
-  useEffect(() => {
-    let mySplitText = new SplitText("#quote", { type: "words,chars" }),
-    chars = mySplitText.chars;
-    gsap.set("#quote", { perspective: 400, opacity: 1 });
-
-    gsap.from(chars, {
-      duration: 0.8,
-      opacity: 0,
-      scale: 0,
-      y: 80,
-      rotationX: 180,
-      transformOrigin: "0% 50% -50",
-      ease: "back",
-      stagger: 0.02,
-      delay: 0.3
-    });
-    
-   
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div ref={para} className="desc-homepage">
@@ -34,11 +12,10 @@ function Description() {
       <div id="quote">
         L'intelligence artificielle est un processus d'imitation de
         l'intelligence humaine qui repose sur la création et l'application
-        d'algorithmes exécutés dans un environnement informatique dynamique. <br></br>Son
-        but est de permettre à des ordinateurs de penser et d'agir comme des
-        êtres humains.{" "}
-      
-    </div>
+        d'algorithmes exécutés dans un environnement informatique dynamique.{" "}
+        <br></br>Son but est de permettre à des ordinateurs de penser et d'agir
+        comme des êtres humains.{" "}
+      </div>
     </div>
   );
 }
