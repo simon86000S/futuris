6f1c19b86b622204c13392c13c0fc92f2900795b
audrain simon <simondu06730@hotmail.fr>
2022-09-29 23:05:39 +0200

    effect

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Description/Description.scss";
+import gsap from "gsap";
+import TextPlugin from "gsap/TextPlugin";
 
 function Description() {
-  
+  gsap.registerPlugin(TextPlugin);
+  useEffect(() => {
+   
 
+    gsap.to("#quote", {
+      duration: 15,
+      text: {
+        value:
+          "L'intelligence artificielle est un processus d'imitation de lintelligence humaine qui repose sur la création et l'application d'algorithmes exécutés dans un environnement informatique dynamique. Son but est de permettre à des ordinateurs de penser et d'agir comme des êtres humains.",
+        delimiter: " ",
+      },
+
+      easy: "none",
+    });
+  }, []);
 
   return (
     <div className="desc-homepage">
-      <h1>L'intelligence artificielle (IA) </h1>
-      <div id="quote">
-        L'intelligence artificielle est un processus d'imitation de
-        l'intelligence humaine qui repose sur la création et l'application
-        d'algorithmes exécutés dans un environnement informatique dynamique.{" "}
-        <br></br>Son but est de permettre à des ordinateurs de penser et d'agir
-        comme des êtres humains.{" "}
-      </div>
+      <h1 >L'intelligence artificielle (IA) </h1>
+      <div id="quote"></div>
     </div>
   );
 }
